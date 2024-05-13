e038b2c170ac25c4f9b2219ea7d95198880f39ee
suksham <129354626+suksham11@users.noreply.github.com>
2024-05-13 20:21:16 +0530

    First commit

---
+++ vite-project/src/Message.tsx
@@ -0,0 +1,12 @@
+
+
+//pascal casing
+function Message(){
+    //jsx: javascript xml
+   const name = '';
+   if(name)
+   return <h1>Hello{name}</h1>
+   return <h1>Hello World</h1>;
+}
+
+export default Message
